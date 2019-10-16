89a69de3676708ba099effd7fc29ea7ee037b81f
AbdelMounhim <AbdelMounhim@users.noreply.github.com>
2019-10-17 00:11:13 +0200

    Update closure.js

--- closure.js
+++ closure.js
@@ -1,6 +1,25 @@
+/*
 
 let i;
-for (i=5; i>=0; i--) {
-  setTimeout( function() { console.log(" i= ",i); }, (5-i)*1000);
+for (i=0; i<5; i++) {
+  console.log("ici i = " , i);
+  setTimeout( function() { console.log(" i= ",i); }, i*1000);
+} 
+
+*/
+
+/*
+function lanceFonction(j) {
+  return setTimeout( function() { console.log("j=",j); }, j*1000 ) ;
 }
+                     
+let i;
+for (i=0; i<5; i++) {
+  lanceFonction(i) 
+ }
+*/
+
+
+
+
 
